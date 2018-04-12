f7266b73bc46b58be6817f2144fac76d3a6f1f47
Alfred H <alfred.hong@twotalltotems.com>
2018-04-11 17:00:35 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ class App extends Component {
     this.state =
       {
         currentData: test1data[0],
-        dataPoint: 0
+        dataPoint: 0,
+        freezingPoint: 0,
+        boilingPoint: 0,
+        difference: 0,
+        direction: 0,
+        units: 'celsius'
+
       }
   }
 
@@ -26,7 +32,7 @@ class App extends Component {
           currentData,
           dataPoint: this.state.dataPoint + 1
         })
-      }.bind(this), 1000)
+      }.bind(this), 100)
     })
   }
 
@@ -34,14 +40,98 @@ class App extends Component {
     clearInterval(this.state.simulation);
   }
 
+  handleDifference = (event) => {
+    this.setState({
+      difference: Number(event.target.value),
+    })
+  }
 
+  handleFreezingChange = (event) => {
+    this.setState({
+      freezingPoint: Number(event.target.value),
+    })
+  }
 
+  handleBoilingChange = (event) => {
+    this.setState({
+      boilingPoint: Number(event.target.value),
 
+    })
+  }
+
+  toggleUnits = () => {
+    this.setState({
+      units: this.state.unit === 'celsius'? 'fahrenheight' : 'celsius'
+    })
+  }
+
+  getfahrenheight = () => {
+    this.setState({
+      units: 'fahrenheight'
+    })
+    return this.state.fahrenheight
+  }
+
+  getCelsius = () =>{
+    this.setState({
+      units: 'celsius'
+    })
+    return this.state.celsius
+  }
+
+  handleDirection = (id) => {
+    this.setState({
+      direction: id
+    })
+}
 
   render() {
+    const {freezingPoint, boilingPoint, difference, direction} = this.state;
+
+    const settings = {
+      freezingPoint,
+      boilingPoint,
+      difference,
+      direction
+    }
+
     return (
       <div className="App">
-        <Thermometer data={this.state.currentData} handleStart={this.handleStart} handleStop={this.handleStop}/>
+        <Thermometer data={this.state.currentData} settings={settings} handleStart={this.handleStart} handleStop={this.handleStop} units={this.state.units}/>
+
+        <div style={{width: '100%'}}>
+          <button onClick={this.handleStart}>Start</button>
+          <button onClick={this.getfahrenheight}>Show Fahrenheight</button>
+          <button onClick={this.getCelsius}>Show Celsius</button>
+          <button onClick={this.handleStop}>Stop</button>
+        </div>
+
+        <div style={{width: '100%'}}>
+          <div>
+            <span style={{marginRight: "30px"}}> Set Freezing Point</span>
+            <input type='number' onChange={this.handleFreezingChange} value={this.state.freezingPoint}/>
+            <button onClick={this.toggleUnits}>{this.state.unit}</button>
+          </div>
+          <div>
+            <span style={{marginRight: "30px"}}> Set Boiling Point</span>
+            <input type='number' onChange={this.handleBoilingChange} value={this.state.boilingPoint}/>
+          </div>
+        </div>
+        <div style={{width: '100%'}}>
+          <div>
+            <span style={{marginRight: "30px"}}> Ignore +/- </span>
+            <input type='number' onChange={this.handleDifference} value={this.state.difference}/>
+            <span style={{marginRight: "30px"}}> changes</span>
+          </div>
+
+          <div>
+            <span style={{marginRight: "30px"}}> Direction </span>
+            <button id={1} onClick={(event) => this.handleDirection(1)}>Ascending</button>
+            <button id={2} onClick={(event) => this.handleDirection(-1)}>Decending</button>
+            <button id={3} onClick={(event) => this.handleDirection(0)}>None</button>
+          </div>
+        </div>
+
       </div>
     );
   }
--- src/thermometer.js
+++ src/thermometer.js
@@ -22,10 +22,8 @@ export default class Thermometer extends React.Component {
       fahrenheight: 0,
       celsius: 0,
       unit: "celsius",
-      freezingThreshold: 0,
-      boilingThreshold: 0,
-      difference: 0,
-      direction: 0
+      freezing: false,
+      boiling: false
     }
     this.setFreezingThreshold.bind(this)
     this.setBoilingThreshold.bind(this)
@@ -49,42 +47,84 @@ export default class Thermometer extends React.Component {
   }
 
   componentWillReceiveProps(nextProps){
-    console.log(nextProps)
-    let {freezing, boiling, direction}  = this.state
-    console.log(direction)
+    const {settings} = nextProps
+    const {boilingPoint, freezingPoint, direction} = settings;
+    //ignore new data if the tempurature is within +/- difference
+    if(Math.abs(nextProps.data.temp - this.state[nextProps.data.unit]) <= this.state.difference) return;
+
+    // check to see if it crosses threshold
+    let {freezing, boiling}  = this.state
+    let oldCelsius = this.state.celsius;
+    let oldFar = this.state.fahrenheight;
     let {celsius,fahrenheight} = this.calcTemp(nextProps.data.unit, nextProps.data.temp);
+    console.log(celsius, fahrenheight,direction)
+    let freezingStatus, boilingStatus;
+
+    switch(direction) {
+      case 1:
+        freezingStatus = freezingPoint <= celsius;
+        boilingStatus = boilingPoint <= celsius;
+        if(this.state.freezing !== freezingStatus) {
+          console.log('crossed freezing threshold')
+          this.setState({
+            freezing: freezingStatus
+          })
+        }
+
+        if(this.state.boiling !== boilingStatus) {
+          console.log('crossed boiling threshold')
+          this.setState({
+            boiling: boilingStatus
+          })
+        }
+        break;
+      case -1:
+        freezingStatus = freezingPoint >= celsius;
+        boilingStatus = boilingPoint >= celsius;
+        if(this.state.freezing !== freezingStatus) {
+          console.log('crossed freezing threshold')
+          this.setState({
+            freezing: freezingStatus
+          })
+        }
+
+        if(this.state.boiling !== boilingStatus) {
+          console.log('crossed boiling threshold')
+          this.setState({
+            boiling: boilingStatus
+          })
+        }
+
+
+        break;
+      case 0:
+        break;
 
 
 
-    if(this.state.unit === 'celsius' && celsius !== this.state.celsius) {
-      freezing = this.checkThreshHold(celsius, this.state.celsius, direction);
-      boiling =  this.checkThreshHold(celsius, this.state.celsius, direction);
     }
+    this.setState({
+      celsius,
+      fahrenheight,
+    })
 
-    else if(this.state.unit === 'fahrenheight' && fahrenheight !== this.state.fahrenheight) {
-      freezing = this.checkThreshHold(fahrenheight, this.state.fahrenheight, direction);
-      boiling =  this.checkThreshHold(fahrenheight, this.state.fahrenheight, direction);
-    }
-      this.setState({
-        celsius,
-        fahrenheight,
-        freezing,
-        boiling
-      })
+
+    if(Math.abs(nextProps.data.temp - this.state[nextProps.data.unit]) <= this.state.difference) return;
   }
 
   calcTemp(from, temp) {
+    let {difference} = this.props.settings
     let celsius = this.state.celsius;
     let fahrenheight = this.state.fahrenheight;
     switch (from) {
       case 'celsius':
-        if(Math.abs(temp - this.state.celsius) > this.state.difference) {
+        if(Math.abs(temp - this.state.celsius) > difference) {
           fahrenheight = temp * 9 / 5 + 32
           celsius = temp
         }
         break
       case 'fahrenheight':
-        if(Math.abs(temp - this.state.fahrenheight) > this.state.difference) {
+        if(Math.abs(temp - this.state.fahrenheight) > difference) {
           celsius = (temp - 32) * (5 / 9)
           fahrenheight = temp
         }
@@ -94,34 +134,6 @@ export default class Thermometer extends React.Component {
     }
     return {celsius, fahrenheight}
 
-  }
-
-  //function called when temperature hits a threshold
-  checkThreshHold (newTemp, prevTemp, direction) {
-
-    switch(direction){
-      case 0:
-        return true
-      case 1:
-         return (newTemp - prevTemp) >= 0
-      case -1:
-        return (prevTemp - newTemp) >= 0
-    }
-
-  }
-
-  getfahrenheight = () => {
-    this.setState({
-      unit: 'fahrenheight'
-    })
-    return this.state.fahrenheight
-}
-
-  getCelsius = () =>{
-    this.setState({
-      unit: 'celsius'
-    })
-    return this.state.celsius
   }
 
   setFreezingThreshold = (value) => {
@@ -136,36 +148,12 @@ export default class Thermometer extends React.Component {
     })
   }
 
-  handleFreezingChange = (event) => {
-    this.setState({
-      freezingThreshold: Number(event.target.value),
-    })
-  }
-
-  handleBoilingChange = (event) => {
-    this.setState({
-      boilingThreshold: Number(event.target.value),
-
-    })
-  }
-
-  handleDifference = (event) => {
-    this.setState({
-      difference: Number(event.target.value),
-    })
-  }
-
-  toggleUnits = () => {
-    this.setState({
-      unit: this.state.unit === 'celsius'? 'fahrenheight' : 'celsius'
-    })
-  }
 
   render () {
     return (
       <div style={{width: '100%'}}>
         <div className={'displayTemp'} style={{width: "100%", textAlign: "center"}}>
-          {this.state.unit === 'celsius' ? `The Temperature is ${this.state.celsius} Degrees Celsius`:
+          {this.props.units === 'celsius' ? `The Temperature is ${this.state.celsius} Degrees Celsius`:
             `The Temperature is ${this.state.fahrenheight} Degrees Fahrenheight`
           }</div>
         <div style={{textAlign: "center"}}>
@@ -176,34 +164,6 @@ export default class Thermometer extends React.Component {
             Freezing - {this.state.freezing ? 'Yes' : "No"}
           </div>
         </div>
-
-        <div style={{width: '100%'}}>
-          <button onClick={this.props.handleStart}>Start</button>
-          <button onClick={this.getfahrenheight}>Show Fahrenheight</button>
-          <button onClick={this.getCelsius}>Show Celsius</button>
-          <button onClick={this.props.handleStop}>Stop</button>
-        </div>
-
-        <div style={{width: '100%'}}>
-            <div>
-              <span style={{marginRight: "30px"}}> Set Freezing Point</span>
-              <input type='number' onChange={this.handleFreezingChange} value={this.state.freezingThreshold}/>
-              <button onClick={this.toggleUnits}>{this.state.unit}</button>
-            </div>
-            <div>
-              <span style={{marginRight: "30px"}}> Set Boiling Point</span>
-              <input type='number' onChange={this.handleBoilingChange} value={this.state.BoilingThreshold}/>
-            </div>
-        </div>
-        <div style={{width: '100%'}}>
-          <div>
-            <span style={{marginRight: "30px"}}> Ignore +/- </span>
-            <input type='number' onChange={this.handleDifference} value={this.state.difference}/>
-            <span style={{marginRight: "30px"}}> changes</span>
-
-          </div>
-        </div>
-
 
       </div>
 
